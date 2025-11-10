601b0ed0ea82b572dfc38dc5a09e6cfd83313617
marsimiklos <mmarsi0114@gmail.com>
2025-11-10 17:16:06 +0100

    Update config_flow.py

--- custom_components/bogyikonyha/config_flow.py
+++ custom_components/bogyikonyha/config_flow.py
@@ -1,7 +1,7 @@
 import logging
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.components import hassio # FONTOS: Hassio importálása
+from homeassistant.components import hassio # Továbbra is szükség van rá a környezet ellenőrzéséhez
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -11,13 +11,13 @@ ADDON_SLUG = "bogyikonya" # Az Add-on azonosítója a manifest.json szerint
 class BogyiKonyhaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Bogyi Konyha Add-on konfigurációs folyamata."""
     
-    VERSION = 2
+    VERSION = 3 # Verziószám növelése a változás jelzésére
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Kezeli a konfiguráció első lépését, ami AUTOMATIKUSAN KINYERI az URL-t."""
         
-        # 1. Csak egy példányt engedélyezünk
+        # 1. Ellenőrzés: Csak egy példányt engedélyezünk
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
@@ -25,23 +25,35 @@ class BogyiKonyhaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if not hassio.is_hassio(self.hass):
             return self.async_abort(reason="not_hassio")
 
+        # ÚJ: Supervisor API kliens beszerzése
         try:
-            # 3. Lekérdezzük az Add-on részletes adatait
-            addon_info = await hassio.async_get_addon_info(self.hass, ADDON_SLUG)
+            supervisor_client = hassio.get_api_client(self.hass)
+        except Exception:
+            # Ha valamiért nem sikerül a kliens beszerzése (ritka)
+            return self.async_abort(reason="supervisor_api_unavailable")
+
+        # 3. Lekérdezzük az Add-on részletes adatait a kliensen keresztül
+        try:
+            # A kliens GET metódusával lekérjük az /addons/{slug}/info végpontot
+            addon_info = await supervisor_client.get(f"addons/{ADDON_SLUG}/info")
             
-            if not addon_info:
+            if not addon_info or not addon_info.get('data'):
                 return self.async_abort(reason="addon_not_found")
             
-            if addon_info["state"] != "started":
+            addon_data = addon_info['data']
+
+            if addon_data.get("state") != "started":
                  return self.async_abort(reason="addon_not_running")
             
             # 4. Összeállítjuk az Add-on belső hálózati címét
-            # Használjuk a Supervisor által biztosított IP-t és a 8099-es portot
-            addon_ip = addon_info["host"] 
+            # Az IP a 'host' mezőben található
+            addon_ip = addon_data["host"] 
+            
+            # A webszerver portja az app.py-ban: 8099
             addon_api_url = f"http://{addon_ip}:8099/api/pantry"
 
         except Exception as err:
-            _LOGGER.error("Hiba az Add-on infó lekérésekor: %s", err)
+            _LOGGER.error("Hiba az Add-on infó lekérésekor a Supervisor kliensen keresztül: %s", err)
             return self.async_abort(reason="addon_info_failed")
             
         # 5. Sikeresen kinyertük az API címet, most létrehozzuk a bejegyzést
